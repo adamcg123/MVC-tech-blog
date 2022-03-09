61c5546e921cf5533c49b071b56740785c74f0fa
adamcg123 <adam.Gondorchin26@gmail.com>
2022-03-09 09:23:54 -0600

    user route just needs the update part otherwise everything else is done

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,56 @@
+const { User } = require("../../models");
+
 const router = require("express").Router();
 
 
 router.get("/", (req, res) => {
-
+  User.findAll({
+    attributes: ["id", "username", "email", "password"]
+  }).then((dbUserData) => {
+    res.json(dbUserData)
+  })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.get("/:id", (req, res) => {
-
+  User.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+    },
+    {
+      attributes: ["id", "username", "email", "password"]
+    })
+    .then((dbUserData) => {
+      if (dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then((dbUserData) => {
+      res.json(dbUserData)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 
 });
 
@@ -18,6 +59,20 @@ router.put("/", (req, res) => {
 });
 
 router.delete("/", (req, res) => {
-
+  User.destroy({
+    where: {
+      id: req.id.params
+    }
+  }).then((dbUserData) => {
+    if (dbUserData) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbUserData)
+  })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 module.exports = router;
